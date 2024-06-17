7ec0a38bfcd02893c2a5f4fa26488966b598f405
Aroult-Govinda <aroult.skysik@gmail.com>
2024-06-17 13:51:18 +0530

    page2 animation

--- script.js
+++ script.js
@@ -43,4 +43,30 @@ function navAnimation() {
 
 navAnimation();
 
+function page2_animation() {
+    var rightElem = document.querySelectorAll(".right-elem")
 
+    rightElem.forEach(function (elem) {
+        elem.addEventListener("mouseenter", function () {
+            gsap.to(elem.childNodes[3], {
+                opacity: 1,
+                scale: 1
+            })
+        })
+        elem.addEventListener("mouseleave", function () {
+            gsap.to(elem.childNodes[3], {
+                opacity: 0,
+                scale: 0
+            })
+        })
+        elem.addEventListener("mousemove", function (dets) {
+
+            gsap.to(elem.childNodes[3], {
+                x: dets.x - elem.getBoundingClientRect().x - 50,
+                y: dets.y - elem.getBoundingClientRect().y - 200,
+            })
+        })
+    })
+}
+
+page2_animation();
